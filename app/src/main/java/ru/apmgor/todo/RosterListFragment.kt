77583b67e499f76204a8a8fffd47338d3765917f
Artur <apmgor@gmail.com>
2021-07-09 16:10:57 +0300

    Add fragment RosterListFragment.kt, rename activity_main.xml to todo_roster.xml

---
+++ app/src/main/java/ru/apmgor/todo/RosterListFragment.kt
@@ -0,0 +1,15 @@
+package ru.apmgor.todo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class RosterListFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.todo_roster, container, false)
+}
