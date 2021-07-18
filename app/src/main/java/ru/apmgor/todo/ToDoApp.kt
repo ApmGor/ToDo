8fd03026d2332925a01715e0c3535ba9f98de2a8
Artur <apmgor@gmail.com>
2021-07-19 02:19:58 +0300

    Add Entity, DAO, Database and new Koin singles

--- app/src/main/java/ru/apmgor/todo/ToDoApp.kt
+++ app/src/main/java/ru/apmgor/todo/ToDoApp.kt
@@ -1,17 +1,28 @@
 package ru.apmgor.todo
 
 import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
+import org.koin.core.qualifier.named
+import org.koin.core.scope.get
 import org.koin.dsl.module
+import ru.apmgor.todo.repo.ToDoDatabase
 import ru.apmgor.todo.repo.ToDoRepository
 import ru.apmgor.todo.ui.SingleModelMotor
 import ru.apmgor.todo.ui.roster.RosterMotor
 
 class ToDoApp : Application() {
     private val koinModule = module {
-        single { ToDoRepository() }
+        single { ToDoRepository(
+            get<ToDoDatabase>().todoStore(),
+            get(named("appScope"))
+        ) }
+        single { ToDoDatabase.newInstance(androidContext()) }
+        single(named("appScope")) { CoroutineScope(SupervisorJob()) }
         viewModel { RosterMotor(get()) }
         viewModel { (modelId: String) -> SingleModelMotor(get(), modelId) }
     }
@@ -21,6 +32,7 @@ class ToDoApp : Application() {
 
         startKoin {
             androidLogger()
+            androidContext(this@ToDoApp)
             modules(koinModule)
         }
     }
---
+++ app/src/main/java/ru/apmgor/todo/repo/TypeTransmogrifier.kt
@@ -0,0 +1,13 @@
+package ru.apmgor.todo.repo
+
+import androidx.room.TypeConverter
+import java.time.Instant
+
+class TypeTransmogrifier {
+    @TypeConverter
+    fun fromInstant(date: Instant?): Long? = date?.toEpochMilli()
+
+    @TypeConverter
+    fun toInstant(millisSinceEpoch: Long?): Instant? =
+        millisSinceEpoch?.let { Instant.ofEpochMilli(it) }
+}
