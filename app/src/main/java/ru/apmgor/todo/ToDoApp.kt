b5e2b14c75286606fc3d0b0ecbe4f1a48292150d
Artur <apmgor@gmail.com>
2021-07-22 15:02:54 +0300

    Adding Handlebars

--- app/src/main/java/ru/apmgor/todo/ToDoApp.kt
+++ app/src/main/java/ru/apmgor/todo/ToDoApp.kt
@@ -1,6 +1,9 @@
 package ru.apmgor.todo
 
 import android.app.Application
+import android.text.format.DateUtils
+import com.github.jknack.handlebars.Handlebars
+import com.github.jknack.handlebars.Helper
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import org.koin.android.ext.koin.androidContext
@@ -13,6 +16,7 @@ import ru.apmgor.todo.repo.ToDoDatabase
 import ru.apmgor.todo.repo.ToDoRepository
 import ru.apmgor.todo.ui.SingleModelMotor
 import ru.apmgor.todo.ui.roster.RosterMotor
+import java.time.Instant
 
 class ToDoApp : Application() {
     private val koinModule = module {
@@ -22,6 +26,18 @@ class ToDoApp : Application() {
         ) }
         single { ToDoDatabase.newInstance(androidContext()) }
         single(named("appScope")) { CoroutineScope(SupervisorJob()) }
+        single {
+            Handlebars().apply {
+                registerHelper("dateFormat", Helper<Instant> { value, _ ->
+                    DateUtils.getRelativeDateTimeString(
+                        androidContext(),
+                        value.toEpochMilli(),
+                        DateUtils.MINUTE_IN_MILLIS,
+                        DateUtils.WEEK_IN_MILLIS, 0
+                    )
+                })
+            }
+        }
         viewModel { RosterMotor(get()) }
         viewModel { (modelId: String) -> SingleModelMotor(get(), modelId) }
     }
